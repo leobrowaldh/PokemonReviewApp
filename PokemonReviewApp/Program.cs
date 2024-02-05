dc8256e7c2ee6e6c993656ef12874e02fa3fdfe7
leobrowaldh <leobrowaldh@gmail.com>
2024-02-05 19:57:33 +0100

    Add project files.

---
+++ PokemonReviewApp/Program.cs
@@ -0,0 +1,62 @@
+
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			builder.Services.AddTransient<Seed>();
+			builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			builder.Services.AddDbContext<DataContext>(
+				options =>
+					options.UseSqlServer(
+						builder.Configuration.GetConnectionString("PokemonConnection")));
+
+			var app = builder.Build();
+
+			if (args.Length == 1 && args[0].ToLower() == "seeddata")
+				SeedData(app);
+
+			void SeedData(IHost app)
+			{
+				var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+				using (var scope = scopedFactory.CreateScope())
+				{
+					var service = scope.ServiceProvider.GetService<Seed>();
+					service.SeedDataContext();
+				}
+			}
+
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
