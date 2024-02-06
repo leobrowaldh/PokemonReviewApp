35896ee03cae55b400004a22f932b0d7dfec5df0
leobrowaldh <leobrowaldh@gmail.com>
2024-02-06 16:33:30 +0100

    pokemon repository get and exist methods implemented

--- PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -12,8 +12,22 @@ public class PokemonRepository : IPokemonRepository
     {
         _context = context;
     }
-    public ICollection<Pokemon> GetPokemons()
+
+	public Pokemon? GetPokemon(int id) => _context.Pokemons.Find(id);
+
+	public Pokemon? GetPokemon(string name) => _context.Pokemons.FirstOrDefault(p => p.Name == name);
+
+	public decimal GetPokemonRating(int pokeId)
+	{
+		var review = _context.Reviews.Where(r => r.Pokemon.Id == pokeId);
+		if(!review.Any()) { return 0}
+		return ((decimal)review.Sum(r => r.Rating)/review.Count());
+	}
+
+	public ICollection<Pokemon> GetPokemons()
     {
         return _context.Pokemons.OrderBy(p => p.Id).ToList();
     }
+
+	public bool PokemonExists(int pokeId) => _context.Pokemons.Any(p => p.Id == pokeId);
 }
