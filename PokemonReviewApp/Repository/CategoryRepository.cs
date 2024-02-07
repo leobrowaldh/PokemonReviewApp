b2d4ae3f83a75cd584529c7353012a025c83776e
leobrowaldh <leobrowaldh@gmail.com>
2024-02-07 14:45:56 +0100

    started on CategoryRepository, decided to switch to generic methods, saving...

---
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository;
+
+public class CategoryRepository : ICategoryRepository
+{
+	private readonly DataContext _context;
+
+	public CategoryRepository(DataContext context)
+    {
+		_context = context;
+	}
+    public bool CategoryExists(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public ICollection<Category> GetCategories()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Category GetCategory(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+	{
+		throw new NotImplementedException();
+	}
+}
